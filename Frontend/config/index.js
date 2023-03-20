a8ef98dcaa2f2fd0d1cacf29e5bb710d6fd7d3a3
Alize.Liu <3152067813@qq.com>
2023-03-20 13:52:57 +0800

    前端路径

--- Frontend/config/index.js
+++ Frontend/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',//本地后端链接地址
         changeOrigin: true,
         pathRewrite: {
            '^/api': '/'
